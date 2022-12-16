4da17c5fd56bbc289dcab6133df5ee718a2412a9
kurus69 <banyuwangi1999@gmail.com>
2022-12-16 10:42:01 +0700

    axios port

--- src/config/api.js
+++ src/config/api.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 export const API = axios.create({
-    baseURL: "http://localhost:5000/waysbucks/",
-    // baseURL: process.env.REACT_APP_BASEURL,
+    // baseURL: "http://localhost:5000/waysbucks/",
+    baseURL: "https://be-waysbucks-production.up.railway.app/waysbucks",
 });
 
 export const setAuthToken = (token) => {
     if (token) {
         API.defaults.headers.common["Authorization"] = `Bearer ${token}`;
     } else {
-        delete API.defaults.headers.commin["Authorization"];
+        delete API.defaults.headers.common["Authorization"];
     }
 };
